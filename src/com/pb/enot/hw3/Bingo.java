614ee5fed7fc76fc86fbf3f0f162ca41c080266a
SASA <sysmaaster@gmail.com>
2021-10-22 23:06:33 +0300

    Bingo ==> Confirm

---
+++ src/com/pb/enot/hw3/Bingo.java
@@ -0,0 +1,57 @@
+package com.pb.enot.hw3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);   // Пакет ввода с Клавиатури
+        Random random = new Random();           //  Пакет Генератора Числа
+        //Масиви та Масонни)
+        int min = 0;                          //мінімальне
+        int max = 100;                       // Максимальне значеня
+        int diff = max - min;               // Формула
+        int hum;                           // число игрока
+        int ii;                           // задуманое число
+        int count = 0;                   // лічильник Спроб
+        char re ;                       // Для питаня про повторну Гру
+
+        //Начнем Гру
+        System.out.println("Игра Угадай число");
+        do { //хочеш начать Новую игру?
+
+            // Загадуєм число от 1 до 100
+            ii = random.nextInt(diff + 1) + min;  //  (max - min) + min;
+            System.out.println(ii);//удачи.Дебагинг.Удалить
+            System.out.println("Я загадал число от 1 до 100. Угадай его!");
+            System.out.println("для виходу використати пароль 666");
+            System.out.println("нет Часу на роздуми");
+            // начать Цикл самої гри
+            do {
+                System.out.println("* Введи число : ");
+                hum = read.nextInt();
+                count++;  // ***
+                //проверка Ответов => Правил
+                if (hum == 666) {  //проверка на Желания вийти. Удалить Игру!
+                    System.out.println("Дякую  за Гру!");
+                    return ;
+                } else if (hum >100 || hum <0) {
+                    System.out.println("Я загадал число только от 1 до 100.");
+                }  else if (ii < hum) {
+                    System.out.println("Не угадал! Моє число меньше.");
+                } else if (ii > hum) {
+                    System.out.println("Не угадал! Моє число больше.");
+                }//повторем цикл до победного!
+            }
+            while (ii != hum);  // Игрок угадал число?
+            System.out.println("Верно. ви угадали!!");
+            System.out.println("Використано >>> "+ count + " спроб!");
+            System.out.println(" Загадать еще число? (+/-)");
+            re = read.next().charAt(0);
+
+        }
+        while (re == '+');// придумать Новое число?
+        System.out.println("Спасибо За Игру");
+        System.out.println("I'm free =) «all rights reserved by sysmaaster»");  // не удалять!!!
+    }
+}
--- src/com/pb/enot/test/test.java
+++ src/com/pb/enot/test/test.java
@@ -1,53 +1,6 @@
-package com.pb.enot.test;
-
-import java.util.Random;
-import java.util.Scanner;
-
-public class test {
-    public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);   // Пакет ввода с Клавиатури
-        Random random = new Random();           //  Пакет Генератора Числа
-        //Масиви та Масонни)
-                       // Формула
-        int hum;                           // число игрока
-        int ii;                           // задуманое число
-
-        //Начнем Гру
-        System.out.println("Игра Угадай число");
-        do { //хочеш начать Новую игру?
-
-            // Загадываем натуральное число
-            // от 1 до 100
-            ii = 55;  //  (max - min) + min;
-            System.out.println(ii);//удачи.Дебагинг.Удалить
-
-            System.out.println("Я загадал число от 1 до 100. Угадай его!");
-
-            do {
-                System.out.println("* Введи число : ");
-                hum = read.nextInt();
-
-                //проверка Ответов
-                if (hum >100 || hum <0) {
-                    System.out.println("Я загадал число только от 1 до 100.");
-                } else if (ii < hum) {
-                    System.out.println("Не угадал! Моё число меньше.");
-                } else if (ii > hum) {
-                    System.out.println("Не угадал! Моё число больше.");
-                } else if (hum == 666) {//проверка на Желания вийти. Удалить Игру!
-                    System.out.println("Не угадал! Моё число больше.");
-                } //повторем цикл до победного!
-            }
-            while (ii != hum);
-            // Игрок угадал число
-            System.out.println("Верно! Загадать еще число? (Y/N)");// как правильно?
-            System.out.println(">>> 666 ");
-            float retr = read.nextFloat();
-        }
-        while (hum == 666);// придумать Новое число?
-        System.out.println("Спасибо За Игру");
-
-        System.out.println("I'm free =) «all rights reserved by sysmaaster»");  // не удалять!!!
-    }
-}
-
+/*
+ завданяя Виконано . . .
+ &#x41E; &#x447; &#x438;
+ &#x449; &#x435; &#x43D;
+ &#x43E; &#x447; &#x438
+*/
