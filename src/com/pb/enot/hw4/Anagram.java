ec3ca89ca3ad358e808ad01eab5ce83917159eb8
SASA <sysmaaster@gmail.com>
2021-10-30 03:29:52 +0300

    Hw4 Push Full! time 3^28

--- src/com/pb/enot/hw4/Anagram.java
+++ src/com/pb/enot/hw4/Anagram.java
@@ -1,20 +1,45 @@
 package com.pb.enot.hw4;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Anagram {
     public static void main(String[] args) {
-        String s  = "@#ц# у ^ 9823l;A ы ^ z a d 9927##$)(^738#";
-        System.out.println("@#ц# у ^ 9823l;A ы ^ z a d 9927##$)(^738#");
-        System.out.println(s=s.replaceAll("[^A-z]", ""));
-        StringTokenizer tok = new StringTokenizer(s,"`~!@#$%^&*()-_+=\\.,><? ");
-        StringBuilder s1 = new StringBuilder();
-        while(tok.hasMoreTokens()){
-            s1.append(tok.nextToken());
+        String tisA1 ;// Створим масив 1
+        String tisA2 ;// Створим масив 2
+        tisA1 = tokenizer(intText(1));
+        tisA2 = tokenizer(intText(2));
+        if (tisA1.compareTo(tisA2) == 0) {
+            System.out.println("Это слово или фраза є Анаграммой");
+        } else {
+            System.out.println("НЕ Анаграмма!");
         }
-        System.out.println(s1);
-
     }
-
-
+    public static String intText(int n) {
+        Scanner in = new Scanner(System.in);
+        String inText;
+        do {
+            System.out.println(" Введите Строку " + n + " :");
+            inText = in.nextLine();///Зберегаєм в Чемодан.
+            if (Objects.equals(inText, "")) {
+                System.out.println("Ой.! Строка пуста( ");
+            }
+        } while (Objects.equals(inText, ""));
+        return inText;
+    }
+    public static String tokenizer(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Zа-яёА-ЯЁ]", "");
+        StringTokenizer tok = new StringTokenizer(s, "`~!@#$%^&*()-—/_+=\\.,><? ");
+        String s1 = "";
+        while (tok.hasMoreTokens()) {
+            s1 = tok.nextToken();
+        }
+        char[] charArray = s1.toCharArray();
+        Arrays.sort(charArray);
+        String sortString = new String(charArray);
+        return sortString;
+    }
 }
--- src/com/pb/enot/hw4/CapitalLetter.java
+++ src/com/pb/enot/hw4/CapitalLetter.java
@@ -5,37 +5,31 @@ import java.util.Scanner;
 
 public class CapitalLetter {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Введите Строку : ");
+        String thisArray;  // Створим масив
+        thisArray = in.nextLine();                ///Зберегаєм в Чемодан.
+        if (Objects.equals(thisArray, "")) {
+            System.out.println("Чемотан Пуст. Что-то пошло не так.!  ПОчиним  !. но можливо це повторитьсяя Знову!)");
+        } else
+            thisArray = thisArray.substring(0, 1).toUpperCase() + thisArray.substring(1).toLowerCase(); //кейс переносим в lowerC.
 
-        for(int i = 0; i < 10;i++) {                      // не обовязкове.!
-            Scanner in = new Scanner(System. in);
-            System.out.println(" Введите Строку : ");
-            String thisArray = new String("");  // Створим масив
-            thisArray = in.nextLine();                ///Зберегаєм в Чемодан.
-            if(Objects.equals(thisArray, "")) {
-                System.out.println("Чемотан Пуст. Что-то пошло не так.!  ПОчиним  !. но можливо це повторитьсяя Знову!)");
-                break;
-            }else//
-            thisArray = thisArray.substring(0,1).toUpperCase() + thisArray.substring(1).toLowerCase(); //кейс переносим в lowerC.
-
-            upperCase(thisArray);                            // Зарос Метода
-        }
+        upperCase(thisArray);
     }
     public static void upperCase(String thisArr) {
-        int em = thisArr.length();                       //знаєм дліну
-        char[] fm1 =new char[em];                       //буффер текста
-        int[] am2 =new int[em];                        //буфф в форматі чар
-        thisArr.getChars(0,em,fm1,0); //Берем кейс переносим в Char. сохраняєм
-        for(int j=1;j<em;j++)                        // Запустим цикл розберем кейс
-        {
-            am2[j]=fm1[j];                         // проверим синхронизицию * дліну
-            if(am2[j-1]==' ' && fm1[j]>96)        // Якщо перед   синволом пробіл.  і символ є Буквой =Тру
-            {  am2[j]=am2[j]-32;   }             // Повишаєм букву в Регистре
-            fm1[j]=(char)am2[j];                // вертаєм значеня Чарс назад в Букву
+        int em = thisArr.length();
+        char[] fm1 = new char[em];
+        int[] am2 = new int[em];
+        thisArr.getChars(0, em, fm1, 0);
+        for (int j = 1; j < em; j++) {
+            am2[j] = fm1[j];
+            if (am2[j - 1] == ' ' && fm1[j] > 96) {
+                am2[j] = am2[j] - 32;
+            }
+            fm1[j] = (char) am2[j];
         }
-        for(int k=0;k<em;k++)
-        {
-            System.out.print(fm1[k]);       // вивод строки результатов
+        for (int k = 0; k < em; k++) {
+            System.out.print(fm1[k]);
         }
-
     }
 }
