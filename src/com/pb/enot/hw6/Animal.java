972a0c20c5a37242aeb59ba2a3da831e815233b9
SASA <sysmaaster@gmail.com>
2021-11-03 00:25:11 +0200

    Hw6 . Creat Structures.

---
+++ src/com/pb/enot/hw6/Animal.java
@@ -0,0 +1,28 @@
+package com.pb.enot.hw6;
+
+public class Animal {
+    String location;
+    String food;
+
+
+    public void makeNoise(){
+        System.out.println("Какой-то звук");
+
+    }
+
+    public void eat(){
+
+
+    }
+
+    public void sleep(){
+        System.out.println("Такое-то животное спит");
+    }
+
+
+
+
+
+
+
+}
---
+++ src/com/pb/enot/hw6/VetClinic.java
@@ -0,0 +1,4 @@
+package com.pb.enot.hw6;
+
+public class VetClinic {
+}
