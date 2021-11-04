d2788c338ad0c164dfdbc9b49a5d08eb99c4d421
SASA <sysmaaster@gmail.com>
2021-11-04 19:17:38 +0200

    Hw6 . Рефлексия I already give up ;(

--- src/com/pb/enot/hw6/VetClinic.java
+++ src/com/pb/enot/hw6/VetClinic.java
@@ -1,23 +1,24 @@
 package com.pb.enot.hw6;
-import java.lang.reflect.*;
+
 public class VetClinic {
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
+
             Veterinarian veterinarian = new Veterinarian();
             Animal[] animals = new Animal[3];
             animals[0] = new Dog("Каша и кости", "Двор частного дома");
-            animals[1] = new Horse("Сено", "Степь" ,66.8);
+            animals[1] = new Horse("Сено", "Степь" ,2.8);
             animals[2] = new Cat("Молоко и рыба", "Квартира");
 
-
-
-            for (Animal animal : animals) {
-                veterinarian.treatAnimal(animal);
-            }
-        }
+         for (Animal animal : animals) { veterinarian.treatAnimal(animal); }
 
 
 
-   /* public class ReflectionCheck {
+   /*
+    Class<?> c = Class.forName("com.pb.enot.hw6.Veterinarian");
+            Object object = c.newInstance();
+            Constructor<?> constr = c.getConstructor(new Class[] {String.class});
+System.out.print(constr);
+    public class ReflectionCheck {
 // new ReflectionCheck();
         public void  s
             Class clazz = object.getClass();
@@ -25,4 +26,4 @@ public class VetClinic {
         }
 
 */
-}
+}}
