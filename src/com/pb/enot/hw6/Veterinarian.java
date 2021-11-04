65ed24c065a7d246533754ae63b2f81795cab150
SASA <sysmaaster@gmail.com>
2021-11-04 18:17:13 +0200

    Hw6 . Ready too Finale?.

---
+++ src/com/pb/enot/hw6/Veterinarian.java
@@ -0,0 +1,8 @@
+package com.pb.enot.hw6;
+
+public class Veterinarian {
+        public void treatAnimal(Animal animal) {
+            System.out.println("---------------");
+            System.out.println(animal);
+        }
+}
