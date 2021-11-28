76c66855a3574653af36771a0b00ba3a993307a0
sysmaster <sysmaster@gmail.com>
2021-11-28 21:09:48 +0200

    hw10 параметризованный класс NumBox

--- src/com/pb/enot/hw10/Main.java
+++ src/com/pb/enot/hw10/Main.java
@@ -2,6 +2,25 @@ package com.pb.enot.hw10;
 
 public class Main {
     public static void main(String[] args) {
+        // 29, 68, 69, 8, 81, 63, 19, 62, 74, 4
+        NumBox<Number> box = new NumBox<>(5);
+        box.add(0, 63);
+        box.add(1, 29);
+        box.add(2, 68);
+        box.add(3, 69);
+        box.add(4, 8);
+        box.add(5, 81);
 
+        // print(box);   //показать масив
+///////////////////////////////////////////
+///////// System.out.println();///////////
+/////////////////////////////////////////
+
+        int index = (int) (Math.random() * box.length());  //
+        System.out.println("Значение елемента " + (index + 1) + " равно " + box.get(index));//возвращающий число по индексу.
+        System.out.println("Размер " + box.length()); // возвращает текущее количество элементов.
+        System.out.println("Среднее значение " + box.average()); // подсчет среднего арифметического среди элементов массива.
+        System.out.println("Сумма: " + box.sum());   // сумма всех элементов массива.
+        System.out.println("Максимум " + box.max());  // - максимальный элемент массива.
     }
 }
--- src/com/pb/enot/hw10/NumBox.java
+++ src/com/pb/enot/hw10/NumBox.java
@@ -1,46 +1,57 @@
 package com.pb.enot.hw10;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class NumBox<T extends Number> {
-    private final T[] nams;
 
-    public NumBox(T[] nams) {
-        this.nams = nams;
+    private final T[] numbers;
+
+    @SuppressWarnings("unchecked")
+    public NumBox(int size) {
+        numbers = (T[]) new Number[size];
     }
 
-//  возвращающий число по индексу.
-    public T get(int index) {
-        return nams[index];
-    }
+    public void add(int index, T num) throws ArrayIndexOutOfBoundsException {
+        try {
 
-    public static void main(String[] args) {
-        List<?> box = Arrays.asList(29, 68, 69, 8, 81, 63, 19, 62, 74, 4);
-        print(box);
-///////////////////////////////////////////
-///////// System.out.println();///////////
-/////////////////////////////////////////
-
-        int index = (int) (Math.random()*5);  //*box.length()
-        System.out.println("Значение елемента "+(index+1)+" равно "+box.get(index));//возвращающий число по индексу.
-
-//  System.out.println("Размер "+box.length());  возвращает текущее количество элементов.
-//  System.out.println("Среднее значение " +box.average()); // подсчет среднего арифметического среди элементов массива.
-//  System.out.println("Сумма: "+box.sum());   // сумма всех элементов массива.
-//  System.out.println("Максимум "+box.max());  // - максимальный элемент массива.
-}
-
-
-
-
-
-
-
-    private static void print(List<?> list) {
-        for (Object obj: list) {
-            System.out.print(obj + ". ");
+            if (index > numbers.length) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else numbers[index] = num;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Массив переполнен! Длина массива  " + length());
         }
+    }
+
+    public T get(int index) {
+        return numbers[index]; //  возвращающий число по индексу.
+    }
+
+    public double sum() {
+        double sume = 0.0;
+        for (T i : numbers) {
+            if (i == null) {
+                sume += 0;
+            } else sume += i.doubleValue();
+        }
+        return sume;
+    }
+
+    public T max() {
+        T maX = numbers[0];
+        for (T number : numbers) {
+            if (number.doubleValue() > maX.doubleValue()) {
+                maX = number;
+            }
+        }
+        return maX;
+    }
+
+    public double average() {
+        return (sum() / length());
+    }
+
+    public double length() {
+        double leng = 0;
+        leng = numbers.length;
+        return leng;
     }
 
 }
